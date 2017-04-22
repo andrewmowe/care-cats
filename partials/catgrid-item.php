b6811ff475e84e6220b264f7f78042c87cf7f264
spencer8 <specer@team-eight.com>
2017-04-22 14:43:41 -0400

    load more news, featured cat images

--- partials/catgrid-item.php
+++ partials/catgrid-item.php
@@ -57,6 +57,14 @@
 
 					$cat_meta = get_post_meta( $post->ID, 'pet_data', true );
 
+					$featured_image = '<img src="' . $cat_meta->images[0]->original_url .'" alt="<?php the_title(); ?>" >';
+					$image_class = 'imgwrap-4-6';
+
+					if(has_post_thumbnail()){
+						$featured_image = get_the_post_thumbnail( $post->ID, 'medium' );
+						$image_class .= " no-crop ";
+					}
+
 					// echo '<pre>';
 					// print_r( $cat_meta );
 					// echo '</pre>';
@@ -64,7 +72,7 @@
 
 			<article class="grid-cat col-12 col-md-6 col-lg-4 py-3">
 
-				<a href="<?php the_permalink(); ?>" class="imgwrap-4-6"><img src="<?php echo $cat_meta->images[0]->original_url; ?>" alt="<?php the_title(); ?>" ></a>
+				<a href="<?php the_permalink(); ?>" class="<?php echo $image_class; ?>"><?php echo $featured_image; ?></a>
 
 				<div class="d-flex justify-content-between align-items-baseline bg-gray-lt px-3 py-2">
 
