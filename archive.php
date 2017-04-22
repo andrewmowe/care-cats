e6f5ade0d110c4f36b0941e393a274c1264caf4f
spencer8 <specer@team-eight.com>
2017-04-22 13:54:36 -0400

    news and news singles

--- archive.php
+++ archive.php
@@ -8,71 +8,38 @@
  */
 
 get_header();
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<?php
-$container   = get_theme_mod( 'understrap_container_type' );
-$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
-?>
+<div class="wrapper pt-0" id="full-width-page-wrapper">
 
-<div class="wrapper" id="archive-wrapper">
+	<header>
+		<?php
+		the_archive_title( '<h1 class="text-center bg-gray-lt divider-header">', '</h1>' );
+		?>
+	</header><!-- .page-header -->
 
-	<div class="<?php echo esc_html( $container ); ?>" id="content" tabindex="-1">
+	<section class="available-cats container-fluid px-0 bg-gray-md">
 
-		<div class="row">
+		<?php wp_nav_menu(
+			array(
+				'theme_location'  => 'care-news-menu',
+				'container_class' => 'container',
+				'container_id'    => '',
+				'menu_class'      => 'category-nav',
+				'fallback_cb'     => '',
+				'menu_id'         => 'news-menu',
+				'walker'          => new WP_Bootstrap_Navwalker(),
+			)
+		); ?>
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check', 'none' ); ?>
+	</section>
 
-			<main class="site-main" id="main">
+	<section class="news-list container my-5" id="news-list">
 
-				<?php if ( have_posts() ) : ?>
+		<?php get_template_part('partials/news', 'list'); ?>
 
-					<header class="page-header">
-						<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
-
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
-						?>
-
-					<?php endwhile; ?>
-
-				<?php else : ?>
-
-					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
-
-				<?php endif; ?>
-
-			</main><!-- #main -->
-
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
-
-		</div><!-- #primary -->
-
-		<!-- Do the right sidebar check -->
-		<?php if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : ?>
-
-			<?php get_sidebar( 'right' ); ?>
-
-		<?php endif; ?>
-
-	</div> <!-- .row -->
-
-</div><!-- Container end -->
+	</section>
 
 </div><!-- Wrapper end -->
 
