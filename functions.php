a9f25eb1a137034c0bc3e52c5b55d289afe58fa6
spencer8 <specer@team-eight.com>
2017-04-23 00:17:16 -0400

    single styles, avalaible cat widget

--- functions.php
+++ functions.php
@@ -78,6 +78,72 @@ function t8_typekit_inline() {
 add_action( 'wp_head', 't8_typekit_inline' );
 
 register_nav_menus( array(
-			'care-secondary-menu' => __( 'Secondary Menu', 'understrap' ),
-			'care-news-menu' => __( 'News Menu', 'understrap' ),
-		) );
+	'care-secondary-menu' => __( 'Secondary Menu', 'understrap' ),
+	'care-news-menu' => __( 'News Menu', 'understrap' ),
+) );
+
+// Enable shortcodes in text widgets
+add_filter('widget_text','do_shortcode');
+
+
+function t8_cats_widget($atts){
+	extract(shortcode_atts(array(
+	  'count' => 3,
+	), $atts));
+
+	$args = array(
+			'post_type' => 'cats',
+			'posts_per_page'	=> $count,
+			'orderby'	=> 'menu_order',
+			'meta_query'	=> array(
+					array(
+						'key'		=> 'availability',
+						'value'		=> 'available',
+						'compare'	=> '='
+					)
+			),
+	);
+
+	$cats = new WP_Query( $args );
+
+	if ( $cats->have_posts() ) : 
+
+		$return_string =  '';
+
+
+		while ( $cats->have_posts() ) : $cats->the_post();
+
+			$cat_meta = get_post_meta( get_the_ID(), 'pet_data', true );
+
+			$featured_image = '<img src="' . $cat_meta->images[0]->original_url .'" alt="' . get_the_title() . '" >';
+			$image_class = 'imgwrap-4-6';
+
+			if(has_post_thumbnail()){
+				$featured_image = get_the_post_thumbnail( $post->ID, 'medium' );
+				$image_class .= " no-crop ";
+			}
+
+			$return_string .= '<div class="grid-cat my-4">';
+
+				$return_string .= '<a href="' . get_the_permalink() . '" class="' . $image_class . '">' . $featured_image . '</a>';
+
+				$return_string .= '<div class="d-flex justify-content-between align-items-baseline bg-gray-lt px-3 py-2">';
+
+					$return_string .= '<h3>' . get_the_title() . '</h3>';
+
+					$return_string .= '<a class="text-link" href="' . get_the_permalink() . '">learn more</a>';
+
+				$return_string .= '</div>';
+
+			$return_string .= '</div>';
+
+		endwhile;
+
+	endif;
+
+	wp_reset_postdata();
+
+	return $return_string;
+
+}
+add_shortcode( 't8-cats-widget', 't8_cats_widget' );
