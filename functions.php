e45ef72f260e3651ee0def6f47c69c48913bfc82
spencer8 <specer@team-eight.com>
2017-04-25 22:33:31 -0400

    added smart versioning for custom css and js

--- functions.php
+++ functions.php
@@ -16,8 +16,11 @@ function theme_enqueue_styles() {
 	// Get the theme data
 	$the_theme = wp_get_theme();
 
-    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
-    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
+	$css_ver = filemtime( get_stylesheet_directory() . '/css/child-theme.min.css' );
+	$js_ver = filemtime( get_stylesheet_directory() . '/js/child-theme.min.js' );
+
+    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $css_ver );
+    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $js_ver, true );
 }
 
 // Custom post types
