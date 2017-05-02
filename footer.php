bb6380a05478c8ab18adfe39fdc96eea5b63d015
Andrew Mowe <andrew@team-eightcom>
2017-05-02 16:29:22 -0400

    added MailChimp form code

--- footer.php
+++ footer.php
@@ -34,12 +34,18 @@ $the_theme = wp_get_theme();
 
 				<h3>Stay Connected</h3>
 				<p>Sign up for our newsletter</p>
-				<form class="form-inline mb-4 row mx-0">
-					<label class="sr-only" for="inlineFormInput">Email</label>
-					<input type="email" class="form-control col-8 m-0" id="inlineFormInput" placeholder="...enter your email">
 
-					<div class="col-4 pl-2 pr-0"><button type="submit" class="btn btn-gray w-100">Sign Up</button></div>
+				<!-- Begin MailChimp Signup Form -->
+				<form action="//care-cats.us15.list-manage.com/subscribe/post?u=5362ba8d348c8a223262e4d3f&amp;id=887cc66615" class="form-inline mb-4 row mx-0 mc4wp-form mc4wp-form-17733" data-id="17735" data-name="Newsletter Signup" id="mc4wp-form-1" method="post" name="mc4wp-form-1">
+						<label class="sr-only" for="inlineFormInput">Email address</label>
+						<input name="EMAIL" class="form-control col-8 m-0" id="inlineFormInput" placeholder="...enter your email" required="" type="email">
+						<div class="col-4 pl-2 pr-0"><button type="submit" class="btn btn-gray w-100">Sign Up</button></div>
+						<div style="display: none;">
+							<input autocomplete="off" name="_mc4wp_honeypot" tabindex="-1" type="text" value="">
+						</div><input name="_mc4wp_timestamp" type="hidden" value="1493741299"><input name="_mc4wp_form_id" type="hidden" value="17735"><input name="_mc4wp_form_element_id" type="hidden" value="mc4wp-form-1">
+					<div class="mc4wp-response"></div>
 				</form>
+				<!-- End MC Signup Form -->
 
 				<div class="d-flex justify-content-between">
 
